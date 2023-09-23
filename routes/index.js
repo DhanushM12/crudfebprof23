7ecb06bc91252ea4ce1f212e4393cbc05ed7cbb7
DhanushM12 <dhanushmukhi@gmail.com>
2023-09-23 14:26:49 +0530

    delete, patch, get

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,51 @@ router.get('/list', async (req, res) => {
     }
 })
 
+router.get('/:id', getStudentData, (req, res) => {
+    res.status(200).json(res.student);
+})
+
+router.patch('/:id', getStudentData, async (req, res) => {
+    if(req.body.name !== undefined){
+        res.student.name = req.body.name;
+    }
+    if(req.body.company !== undefined){
+        res.student.company = req.body.company;
+    }
+    try {
+        const updatedStudent = await res.student.save();
+        return res.status(200).json({updatedStudent})
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+})
+
+router.delete('/:id', getStudentData, async (req, res) => {
+    let name = res.student.name;
+    try {
+        await res.student.deleteOne();
+        return res.json({message: `${name} student has been deleted`})
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+})
+
+async function getStudentData(req, res, next){
+    let student;
+    try {
+        student = await Student.findById(req.params.id);
+        if(student === null){
+            return res.status(404).json({
+                message: "Cannot find the student"
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+    res.student = student;
+    next()
+}
+
 module.exports = router;
